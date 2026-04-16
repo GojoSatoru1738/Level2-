2e4f9b43207c6f666f4f6a53b9028424c550092b
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-04-15 23:05:15 -0400

    YAHHHHHHH

---
+++ game.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var timer;
+var interval = 1000/60;
--- gameobject.js
+++ gameobject.js
@@ -27,3 +27,8 @@ this.drawRect = function() {
     context.fillRect(this.x - this.width/2, this.y - this.height/2, this.width, this.height);
     context.restore();
 };
+
+this.move = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+};
