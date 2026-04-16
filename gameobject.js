0bdf9e38ee62dbeb1216d656b6e2fbde2f93e7e5
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-04-15 23:01:59 -0400

    Rectangle

--- gameobject.js
+++ gameobject.js
@@ -20,3 +20,10 @@ this.drawCircle = function() {
     context.fill();
     context.restore();
 };
+
+this.drawRect = function() {
+    context.save();
+    context.fillStyle = this.color;
+    context.fillRect(this.x - this.width/2, this.y - this.height/2, this.width, this.height);
+    context.restore();
+};
