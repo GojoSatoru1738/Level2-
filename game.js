3f5bc6d93d3c609be3822d887b910469a4c362c5
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-04-15 23:13:29 -0400

    MOREEEE

--- game.js
+++ game.js
@@ -2,3 +2,11 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var timer;
 var interval = 1000/60;
+
+var player1 = new GameObject();
+
+player1.x = 25;
+player1.y = canvas.height/2;
+
+player1.width = 20;
+player1.height = 100;
