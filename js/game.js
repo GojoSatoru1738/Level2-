aa6b6532550a22af6d5fea41a63274329124bfc9
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-04-23 00:21:07 -0400

    Losing

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ var context = canvas.getContext("2d");
 var timer;
 var interval = 1000/60;
 
-//BALL
+
 var ball = new GameObject();
 
 ball.x = canvas.width / 2;
@@ -14,7 +14,7 @@ ball.width = 50;
 ball.height = 50;
 ball.color = "purple";
 
-//PADDLE
+
 var paddle = new GameObject();
 
 paddle.x = 25;
@@ -67,12 +67,12 @@ function animate()
     ball.x += ball.vx;
     ball.y += ball.vy;
 //ye
-    if (ball.collidesWith(paddle)) {
-        ball.vx = -ball.vx;
+    //if (ball.collidesWith(paddle)) {
+       // ball.vx = -ball.vx;
 
         // prevent sticking
-        ball.x = paddle.right() + ball.width / 2;
-    }
+        //ball.x = paddle.right() + ball.width / 2;
+   // }
     
     if (ball.x > canvas.width) {
         ball.x = canvas.width;
@@ -81,10 +81,10 @@ function animate()
     }
 
     
+    
     if (ball.x < 0) {
-        ball.x = 0;
-        ball.x += ball.width/2;
-        ball.vx = -ball.vx;
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
     }
 
     
@@ -102,35 +102,29 @@ function animate()
     }
 
     ball.drawCircle();
+    
+    if (w) {
+        paddle.vy = -5;
+    } 
+    else if (s) {
+        paddle.vy = 5;
+    } 
+    else {
+        paddle.vy = 0;
+    }
 
     paddle.move();
     paddle.drawRect();
 
-    if (player1.y < player1.height/2) {
-    player1.y = player1.height/2;
+    if (paddle.y < paddle.height/2) {
+    paddle.y = paddle.height/2;
 }
 
-if (player1.y > canvas.height - player1.height/2) {
-    player1.y = canvas.height - player1.height/2;
+if (paddle.y > canvas.height - paddle.height/2) {
+    paddle.y = canvas.height - paddle.height/2;
 }
 
-    player1.drawRect();
+   
 }
 
-document.addEventListener("keydown", keyDownHand);
-document.addEventListener("keyup", keyUpHand);
 
-function keyDownHand(e) {
-    if (e.key == "w") {
-        player1.vy = -5;
-    }
-    if (e.key == "s") {
-        player1.vy = 5;
-    }
-}
-
-function keyUpHand(e) {
-    if (e.key == "w" || e.key == "s") {
-        player1.vy = 0;
-    }
-}
