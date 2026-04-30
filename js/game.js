014171493ba73dd4135c4b3a6d642d65f16ef0ab
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-04-30 09:34:56 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -16,21 +16,34 @@ ball.color = "purple";
 
 
 var paddle = new GameObject();
+var paddle2 = new GameObject();
 
 paddle.x = 25;
 paddle.y = canvas.height/2;
 
+paddle2.x = canvas.width - 25;
+paddle2.y = canvas.height/2;
+
 paddle.width = 5;
 paddle.height = 100;
+
+paddle2.width = 5;
+paddle2.height = 100;
 
 timer = setInterval(animate, interval);
 
 document.addEventListener("keydown", press);
 document.addEventListener("keyup", release);
 
+var p1Wins = 0;
+var p2Wins = 0;
+
 
 var w = false;
 var s = false;
+
+var up = false;
+var down = false;
 
 function press(e)
 {
@@ -44,6 +57,14 @@ function press(e)
 	{
 		s = true;
 	}
+    if (e.keyCode == 38)
+    {
+        up = true;
+    }
+    if (e.keyCode == 40)
+    {
+        down = true;
+    }
 }
 
 function release(e)
@@ -58,6 +79,14 @@ function release(e)
 	{
 		s = false;
 	}
+    if(e.keyCode == 38)
+    {
+        up = false;
+    }
+    if(e.keyCode == 40)
+    {
+        down = false;
+    }
 }
 
 function animate()
@@ -110,12 +139,27 @@ function animate()
     paddle.move();
     paddle.drawRect();
 
-    if (paddle.y < paddle.height/2) {
-    paddle.y = paddle.height/2;
+    //paddle 2
+
+     if (up) {
+        paddle2.vy = -5;
+    } 
+    else if (down) {
+        paddle2.vy = 5;
+    } 
+    else {
+        paddle2.vy = 0;
+    }
+
+    paddle2.move();
+    paddle2.drawRect();
+
+    if (paddle2.y < paddle2.height/2) {
+    paddle2.y = paddle2.height/2;
 }
 
-if (paddle.y > canvas.height - paddle.height/2) {
-    paddle.y = canvas.height - paddle.height/2;
+if (paddle2.y > canvas.height - paddle2.height/2) {
+    paddle2.y = canvas.height - paddle2.height/2;
 }
 
 // ALL DA COLLISION
@@ -143,6 +187,35 @@ if (paddle.y > canvas.height - paddle.height/2) {
 
             
             ball.x = paddle.x + paddle.width/2 + ball.width/2;
+        }
+    }
+}
+
+//Paddle 2
+    if (ball.x - ball.width/2 < paddle2.x + paddle2.width/2)
+{
+    if (ball.y > paddle2.y - paddle2.height/2)
+    {
+        if (ball.y < paddle2.y + paddle2.height/2)
+        {
+            
+            if (ball.y < paddle2.y - paddle2.height/6) {
+                ball.vx = 4;
+                ball.vy = -4;
+            }
+            
+            else if (ball.y < paddle2.y + paddle2.height/6) {
+                ball.vx = 4;
+                ball.vy = 0;
+            }
+            
+            else {
+                ball.vx = 4;
+                ball.vy = 4;
+            }
+
+            
+            ball.x = paddle2.x + paddle2.width/2 + ball.width/2;
         }
     }
 }
